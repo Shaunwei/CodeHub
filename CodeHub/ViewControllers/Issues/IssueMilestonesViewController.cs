918a2eefa190609e6896200a41fb6b96cce0d052
Dillon Buchanan <thedillonb@gmail.com>
2013-10-23 20:40:02 -0400

    Fixed issue where you couldn't unset the Milestones

--- CodeHub/ViewControllers/Issues/IssueMilestonesViewController.cs
+++ CodeHub/ViewControllers/Issues/IssueMilestonesViewController.cs
@@ -4,6 +4,7 @@ using CodeHub.Controllers;
 using MonoTouch.Dialog;
 using CodeFramework.ViewControllers;
 using CodeHub.ViewModels;
+using System.Linq;
 
 namespace CodeHub.ViewControllers
 {
@@ -24,11 +25,19 @@ namespace CodeHub.ViewControllers
             SearchPlaceholder = "Search Milestones".t();
             ViewModel = new IssueMilestonesViewModel(user, repo);
 
-            BindCollection(ViewModel.Milestones, x => {
-                return new StyledStringElement(x.Title, () => {
-                    if (MilestoneSelected != null)
-                        MilestoneSelected(x);
-                });
+            //Add a fake 'Unassigned' guy so we can always unassigned what we've done
+            ViewModel.BindCollection(x => x.Milestones, (ev) =>
+            {
+                var items = ViewModel.Milestones.ToList();
+                var noMilestone = new MilestoneModel { Title = "No Milestone".t() };
+                items.Insert(0, noMilestone);
+
+                RenderList(items, x => {
+                    return new StyledStringElement(x.Title, () => {
+                        if (MilestoneSelected != null)
+                            MilestoneSelected(x == noMilestone ? null : x);
+                    });
+                }, ViewModel.Milestones.MoreItems);
             });
         }
     }
